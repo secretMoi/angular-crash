6ae35b08d948f79802a8c9f1c67c4120ef5101d3
quentin.bolsee <quentin.bolsee@lampiris.be>
2021-10-12 11:48:40 +0200

    Add remove task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import { Task } from 'src/app/Task';
 
 
@@ -14,5 +14,10 @@ export class TaskService {
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task) : Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
