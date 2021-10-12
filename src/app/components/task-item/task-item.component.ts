3667ef397f134980469da2336c863d1ac67fa197
quentin.bolsee <quentin.bolsee@lampiris.be>
2021-10-12 11:58:55 +0200

    Add put method

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -13,6 +13,7 @@ export class TaskItemComponent implements OnInit {
   faTimes = faTimes;
 
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
 
   constructor() { }
 
@@ -23,4 +24,7 @@ export class TaskItemComponent implements OnInit {
     this.onDeleteTask.emit(task);
   }
 
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
+  }
 }
